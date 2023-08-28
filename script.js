ad505821433e9b97522505ae7252020e5ca0cc79
Gintas <pikolas666@gmail.com>
2023-08-28 10:47:40 +0300

    added header

---
+++ script.js
@@ -0,0 +1,18 @@
+[
+    {
+      "title": "Tommy Hilfiger TH 1318 VN5",
+      "price": 64,
+      "photo": "https://cdn.alensa.lt/imagesCdn/16356/20577-800x600.webp",
+      "description": "Apie šiuos akinius. Ši forma žinoma kaip katės akių. Ji puikai tinka ovalaus ir keturkampio formos veidams. Mėlyna rėmelių spalva.",
+      "location": "Vilnius",
+      "id": "1"
+    },
+    {
+      "title": "Tommy Hilfiger TH 1531 SX7",
+      "price": 50,
+      "photo": "https://cdn.alensa.lt/imagesCdn/23891/35813-800x600.webp",
+      "description": "Apie šiuos akinius. Ši forma žinoma kaip katės akių. Ji puikai tinka ovalaus ir keturkampio formos veidams. Havana spalvos akinių rėmeliai vienareikšmiškai yra labai modernūs.",
+      "location": "Kaunas",
+      "id": "2"
+    }
+  ]
