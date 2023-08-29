932d0d697398787e9f9daf89e0e0134d5d7e46d6
Gintas <pikolas666@gmail.com>
2023-08-29 14:00:39 +0300

    refactored addCard

--- addCard.js
+++ addCard.js
@@ -1,7 +1,30 @@
 const addBtn = document.getElementById('addBtn');
 const addMessage = document.getElementById('addMessage');
+const validateURL = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/;
+const glassesURL = "https://64ec4552f9b2b70f2bfa0585.mockapi.io/akiniai/";
 
-addBtn.addEventListener('click',()=>{
+const inputValidation = (id, title, price, description, location, photoURL) => {
+    if (!id) {
+        throw new Error('no ID');
+    }
+    if (!title) {
+        throw new Error('no title');
+    }
+    if (!price) {
+        throw new Error('no price');
+    }
+    if (!description) {
+        throw new Error('no description');
+    }
+    if (!location) {
+        throw new Error('no location');
+    }
+    if (!validateURL.test(photoURL)) {
+        throw new Error('bad link');
+    }
+}
+
+const createNewCard = () => {
     const id = document.getElementById('id').value;
     const title = document.getElementById('title').value;
     const price = document.getElementById('price').value;
@@ -9,25 +32,7 @@ addBtn.addEventListener('click',()=>{
     const description = document.getElementById('description').value;
     const location = document.getElementById('location').value;
 
-    const validateURL = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/;
-    if (!id){
-        throw new Error('no ID');
-      }
-    if (!title){
-      throw new Error('no title');
-    }
-    if (!price){
-      throw new Error('no price');
-    }
-    if (!description){
-      throw new Error('no description');
-    }
-    if (!location){
-        throw new Error('no location');
-      }
-    if (!validateURL.test(photoURL)){
-      throw new Error('bad link');
-    }
+    inputValidation(id, title, price, description, location, photoURL);
 
     const newCard = {
         title: title,
@@ -37,35 +42,48 @@ addBtn.addEventListener('click',()=>{
         location: location,
         id: id
     }
-   
-    const addCard = async (newCard) => {
-        try {
-          const response = await fetch(
-            "https://64ec4552f9b2b70f2bfa0585.mockapi.io/akiniai",
-            {
-              method: "POST",
-              headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(newCard),
-            }
-          );
-          if(response){
-            addMessage.setAttribute('style', ' color:#339900;')
-            addMessage.innerHTML = "New Card added successfuly"
-            setTimeout(() => {
-                window.location.replace("./index.html");
-              }, 3000);
-          } else {
-            addMessage.setAttribute('style', 'color: red')
-            addMessage.innerHTML = "Failed to add a New Card"
-          }
-        } catch (err) {
-          return false;
-        }
-      };
-      addCard(newCard)
-     
-});
 
+    return newCard;
+}
+
+const insertCard = async (newCard) => {
+    try {
+      const response = await fetch(glassesURL,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newCard),
+        }
+      );
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      return false;
+    }
+  };
+
+  const onCardInserted = (data) => {
+    if(data){
+        addMessage.setAttribute('style', ' color:#339900;')
+        addMessage.innerHTML = "New Card added successfuly"
+        setTimeout(() => {
+            window.location.replace("./index.html");
+          }, 3000);
+      } else {
+        addMessage.setAttribute('style', 'color: red')
+        addMessage.innerHTML = "Failed to add a New Card"
+      }
+    }
+
+addBtn.addEventListener("click", async () => {
+    try {
+      const newCard = createNewCard();
+      const data = await insertCard(newCard);
+      onCardInserted(data);
+    } catch (err) {
+      console.log("err", err);
+    }
+  });
